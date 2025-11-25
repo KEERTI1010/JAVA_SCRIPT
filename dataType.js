33a962f83c9452c92e7d3589e91d0143d40c82f3
KEERTI1010 <shetkeerti6@gmail.com>
2025-11-25 18:07:55 +0530

    dataType.

---
+++ dataType.js
@@ -0,0 +1,35 @@
+"use strict";//treat all JS code as newer version
+
+//alert("This is data type file");
+//alert(3+3)
+
+console.log(3+3) 
+console.log("KEERTI")
+
+
+//data types
+//eg
+let name = "Keerti"; //string literal
+let age = 24; //number literal
+let isApproved = true; //boolean literal
+let person = null; //null literal
+let state; //undefined literal
+
+
+//many of them are in that
+//number => 1,2,3,4,5,6
+//string => "Keerti", 'Keerti', `Keerti`
+//boolean => true,false
+//null => null(standalone value)
+//undefined => undefined
+//symbol => unique identifiers
+//bigint => larger integers
+
+//object
+
+console.log(typeof age); //string
+console.log(typeof name); //number
+console.log(typeof isApproved);//boolrean
+console.log(typeof person);//object
+console.log(typeof state);//undefined   
+console.log(typeof null);//object
