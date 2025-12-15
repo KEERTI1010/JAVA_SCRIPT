ffe25a21a3fe062187eae96010929cee7fee08ac
KEERTI1010 <shetkeerti6@gmail.com>
2025-12-15 17:52:01 +0530

    for in loop

---
+++ Iterate_05/four.js
@@ -0,0 +1,17 @@
+// for in loop
+ 
+const myObject = {
+    js : "JAVASCRIPT",
+    cpp : "C++",
+    swift : "SWIFT BY APPLE"
+}
+
+for (const key in myObject) {
+    console.log([key]);
+    
+}   //[ 'js' ]
+    //[ 'cpp' ]
+    //[ 'swift' ]
+
+
+    // so for arrays we use for in loop
